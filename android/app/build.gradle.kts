70c5a8774535fd4e6f74d2acf210b19d695f4dad
zhangzhongnan928 <dev@openpasskey.com>
2026-07-22 14:13:02 +1000

    Prepare v0.1.8 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.openpasskey.terminal"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "0.1.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
